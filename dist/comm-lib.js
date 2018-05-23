c178d1602f71c7400ab53a3d2ac02b891e421b2a
Carl-Ola Boketoft <calleboketoft@gmail.com>
2018-05-23 10:54:35 +0200

    include dist in build

---
+++ dist/comm-lib.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function initComms() {
+    console.log('alright');
+}
+exports.initComms = initComms;
+//# sourceMappingURL=comm-lib.js.map
