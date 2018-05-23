53ad698c3540710159e33a20dd183f10b2b1b303
Carl-Ola Boketoft <calleboketoft@gmail.com>
2018-05-23 11:10:44 +0200

    cleaner interface

--- dist/plugin-comm.js
+++ dist/plugin-comm.js
@@ -1,7 +1,31 @@
 "use strict";
+// This is the API between the platform and the plugins
 Object.defineProperty(exports, "__esModule", { value: true });
-function initComms() {
-    console.log('alright');
+var pluginComm = {
+    // Plugins use this method to send data to the platform
+    sendDataToPlatform: function (dataToPlatform) {
+        this.platformCallback(dataToPlatform);
+    },
+    // Platform use this method to send data to plugins
+    sendDataToPlugin: function (pluginName, dataToPlugin) {
+        this.pluginsCallbacks[pluginName](dataToPlugin);
+    },
+    // Plugins use this method to register their callback method for handling
+    // incoming data from platform
+    registerPluginDataCallback: function (pluginName, cb) {
+        this.pluginsCallbacks[pluginName] = cb;
+    },
+    // Platform uses this method to register a callback to handle incoming
+    // data from plugins
+    registerPlatformDataCallback: function (cb) {
+        this.platformCallback = cb;
+    },
+    pluginsCallbacks: {},
+    platformCallback: null
+};
+if (!window['pluginComm']) {
+    window['pluginComm'] = pluginComm;
 }
-exports.initComms = initComms;
+var pluginCommInstance = window['pluginComm'];
+exports.pluginComm = pluginCommInstance;
 //# sourceMappingURL=plugin-comm.js.map
--- src/plugin-comm.ts
+++ src/plugin-comm.ts
@@ -1,3 +1,36 @@
-export function initComms() {
-  console.log('alright');
+// This is the API between the platform and the plugins
+
+const pluginComm = {
+  // Plugins use this method to send data to the platform
+  sendDataToPlatform: function(dataToPlatform) {
+    this.platformCallback(dataToPlatform);
+  },
+
+  // Platform use this method to send data to plugins
+  sendDataToPlugin: function(pluginName, dataToPlugin) {
+    this.pluginsCallbacks[pluginName](dataToPlugin);
+  },
+
+  // Plugins use this method to register their callback method for handling
+  // incoming data from platform
+  registerPluginDataCallback(pluginName, cb) {
+    this.pluginsCallbacks[pluginName] = cb;
+  },
+
+  // Platform uses this method to register a callback to handle incoming
+  // data from plugins
+  registerPlatformDataCallback(cb) {
+    this.platformCallback = cb;
+  },
+
+  pluginsCallbacks: {},
+
+  platformCallback: null
+};
+
+if (!window['pluginComm']) {
+  window['pluginComm'] = pluginComm;
 }
+
+const pluginCommInstance = window['pluginComm'];
+export { pluginCommInstance as pluginComm };
